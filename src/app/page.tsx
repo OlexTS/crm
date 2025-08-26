14f0d821fbbe3ce7cf14631f35e1490078bc3218
Olexandr <sashokseven@gmail.com>
2025-08-26 21:03:22 +0300

    training with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import ActiveLabel from './components/ActiveLabel';
-import NotActiveLabel from './components/NotActiveLabel';
+import StatusLabel, { Status } from './components/StatusLabel';
 
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
-      <ActiveLabel>Active</ActiveLabel>
-      <NotActiveLabel>Not active</NotActiveLabel>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </div>
   );
 }
